ef4ce10b68b733ddc98015f9ffcab83f583e77a7
Jun <cmoonjun11@gmail.com>
2022-12-21 02:20:34 +0900

    set password encrypt

--- models/Users.js
+++ models/Users.js
@@ -1,4 +1,6 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 // 몽고디비에서 모델은 테이블, 스키마는 컬럼으로 이해하자
 
@@ -30,6 +32,27 @@ const userSchema = mongoose.Schema({
     },
     tokenExp: {
         type: Number
+    }
+})
+
+userSchema.pre('save', function(next){
+    var user = this;
+
+
+    if(user.isModified('password')) { // 유저가 암호를 변경할 때만 암호화
+        // 비밀번호 암호화
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if(err) return next(err)
+    
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if(err) return next(err)
+                user.password = hash;
+                next();
+                // hash는 암호화 된 비밀번호
+            });
+        });    
+    } else {
+        next()
     }
 })
 
